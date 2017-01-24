d47b05485d134f9b24ede0c826d56e436ce369b1
Riadh HAJ AMOR (RH030971) <Riadh.HajAmor@cerner.com>
2017-01-24 18:04:36 +0100

    yet trying to implement a generic action handler

--- header/core/expert/action.hpp
+++ header/core/expert/action.hpp
@@ -1,41 +1,58 @@
 
 #include <boost/tuple/tuple.hpp>
+#include <iostream>
+#include <string>
+#include <type_traits>
 #include <functional>
+#include <boost/fusion/functional/invocation/invoke.hpp>
 
 namespace rete_cpp
 {
 
 
+namespace details{
+    
+template<bool activate, typename T, typename... args>
+struct apply_impl;
 
-template<typename T, typename enabled=void, typename...P>
-struct action{
-     template<P...args> struct apply{ };
-};
+template< typename T,typename... args>
+struct apply_impl<false,T, args...>{};
 
-template<typename FN,  typename...P>
-struct action <FN,  typename std::enable_if< std::is_function<FN>::value >::type, P... >{
-    template<P...args> struct apply{ 
-            static const decltype(std::result_of<FN(P...)>::type) value = FN(...args);
+template< typename FN,typename... P>
+struct apply_impl<true,FN, P... args>{
+       static const  decltype( std::result_of<FN(P...)>::type ) value = FN( std::forward<P>(args)...);
     };
-};
 
-template<typename T, typename enabled=void>
+}
+template<typename T, bool enabled=false, typename ...P>
 struct action
 {
+    template<P... args>
+    using apply=details::apply_impl<enabled,T, args...> ;
+}; 
 
-std::function<void (Types...)> f;
-std::tuple<Types...> args;
 
- 
-    void execute(Types...args){}   
-    static const std::size_t value = sizeof...(Types);
+template<typename FN, typename ...P>
+struct action<FN, typename std::enable_if<std::is_function<FN>::value, bool>{} ,P...>{
+    template<P... args>
+    using apply=details::apply_impl<FN, args...> ;
 };
 
-template<typename obj>
-struct modify : public action<obj>
+/*
+template<class T, class...P>
+using action = details::action_helper<T,P...>;
+*/
+int test(int x)
 {
-    
-};
+    return x;
+}
 
+
+int main()
+{
+ auto al= action<decltype(test), true,int>::apply<5>::value;
+  std::cout << "result :" << al <<std::endl;
+  return 0;
+}
 
 }
