3c5c0bdbd011f380adeee15177cd574a996fa4d3
Riadh HAJ AMOR (RH030971) <Riadh.HajAmor@cerner.com>
2017-02-06 18:17:51 +0100

    fixed cmake conf and started expression impl

---
+++ header/core/expert/expression.hpp
@@ -0,0 +1,30 @@
+// Include all of Proto
+#include <boost/proto/proto.hpp>
+//#include <boost/phoenix/
+
+// Create some namespace aliases
+namespace mpl = boost::mpl;
+namespace fusion = boost::fusion;
+namespace proto = boost::proto;
+namespace phoenix = boost::phoenix;
+
+// Allow unqualified use of Proto's wildcard pattern
+using proto::_;
+
+
+namespace sbre_cpp
+{
+
+    namespace details
+    {
+        struct _expr : std::integral
+        struct make_minus
+        
+
+
+    }
+
+
+
+
+}
---
+++ header/readme.md
@@ -0,0 +1,10 @@
+# Intorduction
+TBD
+# USAGE
+TBD
+
+`#include <rete.hpp>`
+
+`sbre::rete<> Rete;`
+
+`std::tuple<Rule,std::pair<fact, condition>> conflictSet = Rete.build();`
