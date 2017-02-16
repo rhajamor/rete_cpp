8b3978dda5e46a0e08b98b435617c2c6b4c15c57
Riadh HAJ AMOR (RH030971) <Riadh.HajAmor@cerner.com>
2017-02-16 16:03:22 +0100

    fixed quaternion multiplication

--- include/core/expert/math/quaternion.hpp
+++ include/core/expert/math/quaternion.hpp
@@ -72,56 +72,47 @@ struct vect3
     vect3(vect3<fp_type> &&v) : _array(v._array) {}
     vect3() : _array({0, 0, 0}){};
 
-  template <typename = typename std::enable_if<std::is_floating_point<scalar_type>::value, vect3<fp_type>>::type>
-   operator*(const scalar_type& scalar)
+   
+    template<typename scalar_type,
+    typename = typename std::enable_if<std::is_floating_point< scalar_type>::value>::type
+    >
+    vect3<fp_type> operator*(const scalar_type scalar) const
     {
         vect3<fp_type> ret;
         ret._array = {
             _array[0] * scalar,
             _array[1] * scalar,
-            _array[2] * scalar
-            };
+            _array[2] * scalar};
         return std::move(ret);
     }
-    
- vect3<fp_type> mul(const scalar_type &vect1, std::true_type) 
-  {
-         vect3<fp_type> ret;
-        ret._array = {
-            _array[0] * scalar,
-            _array[1] * scalar,
-            _array[2] * scalar
-            };
-        return std::move(ret);
-  } 
-
- fp_type mul(const vect3<fp_type> &vect1, std::false_type) 
-  {
+  
+    fp_type  operator*(const vect3<fp_type> &vect1) const
+    {
         vect3<fp_type> ret;
         std::transform(vect1._array.begin(), vect1._array.end(), _array.begin(), ret._array.begin(), std::multiplies<fp_type>());
         fp_type init;
         return std::accumulate(ret._array.begin(), ret._array.end(), init, std::plus<fp_type>());
-  } 
+    }
 
-    vect3<fp_type> operator-(const vect3<fp_type> &vect1)
+
+    vect3<fp_type> operator-(const vect3<fp_type> &vect1) const
     {
         vect3<fp_type> ret;
         std::transform(vect1._array.begin(), vect1._array.end(), _array.begin(), ret._array.begin(), std::minus<fp_type>());
         return std::move(ret);
     }
 
-    vect3<fp_type> operator^(const vect3<fp_type> &vect1)
+    vect3<fp_type> operator^(const vect3<fp_type> &vect1) const
     {
         vect3<fp_type> product;
         product._array =
-        { _array[1] * vect1[2] - _array[2] * vect1[1],
-          _array[2] * vect1[0] - _array[0] * vect1[2],
-          _array[0] * vect1[1] - _array[1] * vect1[0]
-        };
+            {_array[1] * vect1[2] - _array[2] * vect1[1],
+             _array[2] * vect1[0] - _array[0] * vect1[2],
+             _array[0] * vect1[1] - _array[1] * vect1[0]};
         return std::move(product);
     }
 
-    vect3<fp_type> operator+(const vect3<fp_type> &vect1)
+    vect3<fp_type> operator+(const vect3<fp_type> &vect1) const
     {
         vect3<fp_type> ret;
         std::transform(vect1._array.begin(), vect1._array.end(), _array.begin(), ret._array.begin(), std::plus<fp_type>());
@@ -136,7 +127,7 @@ struct vect3
         return *this;
     }
 
-    fp_type operator[](const int index)
+    fp_type operator[](const int index) const
     {
         return _array[index];
     }
@@ -151,7 +142,7 @@ struct Quaternion
     Quaternion(Quaternion<fp_type> &q) : w(std::move(q.w)), v(std::move(q.v)) {}
     Quaternion(Quaternion<fp_type> &&q) : w(q.w), v(q.v) {}
 
-    Quaternion<fp_type> operator*(const Quaternion<fp_type> &q)
+    Quaternion<fp_type> operator*(const Quaternion<fp_type> &q) const
     {
         /* a_=(a_*q.a_) -(b_.real()*q.b_.real())-(c_.real()*q.c_.real())-(d_.real()*q.d_.real());
     b_= ( (a_*q.b_.real()) + (b_.real()*q.a_) + (c_.real()*q.d_.real()) -(d_.real()*q.c_.real())  );
@@ -160,10 +151,7 @@ struct Quaternion
     */
         Quaternion<fp_type> q1;
         q1.w = w * q.w - (v * q.v).real();
-        auto x_1 = v * q.w;
-        auto x_2 = q.v * w;
-        auto x_3 = (q.v ^ v);
-        q1.v = (x_1 + x_2) + x_3;
+        q1.v = (v * q.w +  q.v * w) + (q.v ^ v);
         return std::move(q1);
     }
 
@@ -180,15 +168,12 @@ struct Quaternion
 
 int main()
 {
-    vect3<std::complex<float>> v;
-    v._array = {1.75f, -4.f, -5.f};
-    Quaternion<float> q1(3.f, v);
+    vect3<std::complex<double>> v;
+    v._array = {1.75, -4., -5.};
+    Quaternion<double> q1(3, v);
     auto prod = (q1 * q1);
-    std::cout << "prod.w = " << prod.w << "prod.x = " << prod.v[0] << "prod.y = " << prod.v[1] << "prod.z = " << prod.v[1] << "!\n";
-/*
- auto xs=v*v;
- float a=3.2;
- auto xv= v*a;
- */
+    std::cout << "prod.w = " << prod.w << " prod.x = " << prod.v[0] << " prod.y = " << prod.v[1] << " prod.z = " << prod.v[2] << " !\n";
+
+ 
     return 0;
 }
